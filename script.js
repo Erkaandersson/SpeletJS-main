7b83cc056ba54467312cb7f45e1119c42adddb69
Erkaandersson <Erik.andersson.94@hotmail.com>
2023-11-21 10:19:45 +0100

    a

---
+++ script.js
@@ -0,0 +1,25 @@
+const cards = document.querySelector('.cards')
+
+
+function revealImg (inputId){
+    
+    let child = inputId.children[1];
+   
+    if(child.style.display === 'none'){
+        child.style.display = 'flex'
+        
+    }
+    else{
+        child.style.display = 'none'
+    } 
+}
+
+
+
+cards.addEventListener('click', (e) => {
+    revealImg(e.target)
+   
+})
+
+
+
